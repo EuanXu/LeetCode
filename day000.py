cde14825a0aa6470136017ec097a86f8cfb8061e
EuanXu <44606357+EuanXu@users.noreply.github.com>
2020-10-03 20:43:33 +0800

    Initial commit

---
+++ day000.py
@@ -0,0 +1,30 @@
+"""
+1.两数相和
+给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+    给定 nums = [2, 7, 11, 15], target = 9
+    因为 nums[0] + nums[1] = 2 + 7 = 9
+    所以返回 [0, 1]
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(0, len(nums)):
+            num = target - nums[i]
+            if num not in dic:
+                dic[nums[i]] = i
+            else:
+                return [dic[num], i]
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum(nums=[2, 7, 11, 15], target=9))
